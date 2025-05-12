2c5218947896c25963ed893a7728d3cf3769a7dd
Yara-56 <yaramoraes0280@gmail.com>
2025-05-12 08:51:26 -0300

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "./index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import Primeiro from "./components/basicos/primeiro";
+import Primeiro from "./components/basicos/Primeiro";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
