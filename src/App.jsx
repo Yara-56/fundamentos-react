13ab77f79261e364e9751c855217912896547b22
Yara-56 <yaramoraes0280@gmail.com>
2025-05-13 08:39:52 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
+import Card from "./components/basicos/layout/Card"; 
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
+import Aleatorio from "./components/basicos/Aleatorio";
 
 export default (_) => (
   <div id="app">
     <h1>Fundamentos React </h1>
+
+    <Card> titulo="Exemplo de card"
+      
+    </Card>
+
+    <Aleatorio min={1} max={60} />
     <Fragmento />
     <ComParametro titulo="Situacao do Aluno" aluno=" Pedro Silva" nota={9.3} 
     />
